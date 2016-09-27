2e0edabab3737a4cb68f709374112430d9239d17
gidbalugo <zidgyril@gmail.com>
2016-09-27 20:04:39 +0800

    use jdk 7

--- java/attributes/default.rb
+++ java/attributes/default.rb
@@ -19,7 +19,7 @@
 
 # default jdk attributes
 default['java']['install_flavor'] = "openjdk"
-default['java']['jdk_version'] = '6'
+default['java']['jdk_version'] = '7'
 default['java']['arch'] = kernel['machine'] =~ /x86_64/ ? "x86_64" : "i586"
 
 case platform
--- java/metadata.rb
+++ java/metadata.rb
@@ -4,21 +4,13 @@ license           "Apache 2.0"
 description       "Installs Java runtime."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.4.0"
-name 				"java"
-description 		"Install Oracle Java"
+name 			  "java"
 
-default_attributes(
-  "java" => {
-    "install_flavor" => "oracle",
-    "jdk_version" => "7",
-    "oracle" => {
-      "accept_oracle_download_terms" => true
-    }
-  }
-)
-run_list(
-  "recipe[java]"
-)
+recipe "java", "Installs Java runtime"
+recipe "java::openjdk", "Installs the OpenJDK flavor of Java"
+recipe "java::oracle", "Installs the Oracle flavor of Java"
+recipe "java::oracle_i386", "Installs the 32-bit jvm without setting it as the default"
+
 
 %w{ debian ubuntu centos redhat scientific fedora amazon arch freebsd }.each do |os|
   supports os
