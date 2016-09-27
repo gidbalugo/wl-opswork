d1725b8c8cc92c4e03d2f66abdd15aa9fd8754bb
gidbalugo <zidgyril@gmail.com>
2016-09-27 19:33:32 +0800

    test remove java

--- activemq/metadata.rb
+++ activemq/metadata.rb
@@ -5,10 +5,7 @@ description      "Installs activemq and sets it up as service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.2"
 name 			 "activemq"
+
 %w{ubuntu debian redhat centos}.each do |os|
   supports os
-end
-
-%w{java}.each do |cb|
-  depends cb
 end
