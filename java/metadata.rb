859cb00c6116fd89033e87c33bde5f7d05aafb80
gidbalugo <zidgyril@gmail.com>
2016-09-27 19:50:27 +0800

    test fix for java cookbook

--- java/metadata.rb
+++ java/metadata.rb
@@ -4,13 +4,21 @@ license           "Apache 2.0"
 description       "Installs Java runtime."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.4.0"
-name 			  "java"
+name 				"java"
+description 		"Install Oracle Java"
 
-recipe "java", "Installs Java runtime"
-recipe "java::openjdk", "Installs the OpenJDK flavor of Java"
-recipe "java::oracle", "Installs the Oracle flavor of Java"
-recipe "java::oracle_i386", "Installs the 32-bit jvm without setting it as the default"
-
+default_attributes(
+  "java" => {
+    "install_flavor" => "oracle",
+    "jdk_version" => "7",
+    "oracle" => {
+      "accept_oracle_download_terms" => true
+    }
+  }
+)
+run_list(
+  "recipe[java]"
+)
 
 %w{ debian ubuntu centos redhat scientific fedora amazon arch freebsd }.each do |os|
   supports os
