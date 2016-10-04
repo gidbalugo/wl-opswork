5f1e4fd667db97681cf9b5ffa634287be6d798b9
gidbalugo <zidgyril@gmail.com>
2016-10-04 14:32:49 +0800

    added testactivemq that is working with kitchen test

---
+++ testactivemq/attributes/default.rb
@@ -0,0 +1,8 @@
+
+default['activemq']['mirror']  = "http://apache.mirrors.tds.net"
+default['activemq']['version'] = "5.13.4"
+default['activemq']['home']  = "/opt"
+default['activemq']['wrapper']['max_memory'] = "512"
+
+default['activemq']['zooKeeper']['address'] = "localhost:2181"
+default['activemq']['zooKeeper']['hostname'] = "localhost"
